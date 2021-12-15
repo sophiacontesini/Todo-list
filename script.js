b28dfc3d23d39d4bc582ba07af02bce6d75ced0c
sophiacontesini <sophiamarques214@gmail.com>
2021-12-15 14:55:24 -0300

    requisitos 9 e 10 ok

--- script.js
+++ script.js
@@ -4,9 +4,11 @@
 // Itens criados devem permanecer na lista na medida em que novos são adicionados
 // Recebi ajuda da Débora Serra- Turma 19-tribo B
 
-let lista = document.querySelector("ol");
-let botao = document.querySelector ("#criar-tarefa");
-let input = document.querySelector ("input")
+let lista = document.querySelector('ol');
+let botao = document.querySelector ('#criar-tarefa');
+let input = document.querySelector ('input');
+let botaoApaga = document.querySelector('#apaga-tudo');
+let botaoRemove = document.querySelector('#remover-finalizados');
 
 botao.addEventListener("click", criaClick);
   function criaClick() {
@@ -17,8 +19,9 @@ botao.addEventListener("click", criaClick);
     input.value = '';
     criaLista.addEventListener('click', changeColor);
     criaLista.addEventListener('dblclick', riscaItem);
+  
   }
-
+  
 //7. Alterar a cor de fundo para cinza
 //8. Não selecionar mais de um elemento ao mesmo tempo
 // Recebi ajuda da Débora Serra- Turma 19-tribo B
@@ -41,12 +44,18 @@ e.target.classList.toggle('completed');
 
 //10. Adicionar botao com id= "apaga-tudo" 
 // Recebi ajuda da Débora Serra - Turma 19-tribo B
+botaoApaga.addEventListener('click', apagaTudo);
+function apagaTudo(){
+  let itensLista = document.querySelectorAll('li');
+  for(let i=0; i < itensLista.length; i+=1){
+   lista.removeChild(itensLista[i])
+  }
+}
 
 
 //11.  Adicionar botao com id= "remover-finalizados"
 // Recebi ajuda da Débora Serra - Turma 19-tribo B
+botaoRemove.addEventListener('click', removeItem);
+function removeItem(){
 
-
-
-
-
+}
