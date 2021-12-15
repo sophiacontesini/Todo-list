e49dfec83b905b96905f978e30cecc8e2191891c
sophiacontesini <sophiamarques214@gmail.com>
2021-12-15 12:07:36 -0300

    requisitos 7 e 8 ok

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 // 5. Texto digitado no input deve aparecer na lista e desaparecer do input
+// 6. Ordenar os itens por ordem de criação
 // Itens criados devem permanecer na lista na medida em que novos são adicionados
+// Recebi ajuda da Débora Serra- Turma 19-tribo B
 
 let lista = document.querySelector("ol");
 let botao = document.querySelector ("#criar-tarefa");
@@ -12,26 +14,21 @@ botao.addEventListener("click", criaClick);
     let valor = input.value;
     criaLista.innerText = valor;
     lista.appendChild(criaLista);
-    input.value = "";
-
+    input.value = '';
+    criaLista.addEventListener('click', changeColor);
   }
 
-
-
-
-
-// 6. Ordenar os itens por ordem de criação
-
-
-
 //7. Alterar a cor de fundo para cinza
-// texto.addEventListener ("click", corFundo); Evento de click adicionado na função anterior
-// Assiti na mentoria
-
-
-
 // 8. Não selecionar mais de um elemento ao mesmo tempo
+// texto.addEventListener ("click", corFundo); Evento de click adicionado na função anterior
 
+function changeColor(e) {
+  let list = document.querySelectorAll('li');
+  for (let i = 0; i < list.length; i+=1) {
+    list[i].style.backgroundColor = "#d2691e";
+  }
+  e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
 
 // 9. Ao clicar duas vezes em um item: riscá-lo
 // Desfazer essa acao ao clicar duas vezes novamente no item
